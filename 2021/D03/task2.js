2da6038eb56662b1bae61f003d01c16c781b4ade
Michel Tosu <michel@waitwhile.com>
2021-12-05 20:12:11 +0100

    solve 2021 D03 T2

--- 2021/D03/task2.js
+++ 2021/D03/task2.js
@@ -1,40 +1,36 @@
 const { expectation } = require('sinon');
 const { getInputForDay } = require('../../util/InputFetcher');
-//    return Object.keys(charCounts).reduce((prev, next) => mostCommon[prev] > mostCommon[next] ? prev : next);
 
 const histogram = (inputArray) => {
     return inputArray.reduce((prev, next) => {
-        return {...prev, ...{[next]: (prev[next] ?? 0) + 1}};
+        return { ...prev, ...{ [next]: (prev[next] ?? 0) + 1 } };
     }, {});
-
 }
 
 async function main() {
     const input = await (await getInputForDay('2021', '03')).split('\n');
-    
-    let oxygenRating = input.map(val => {return {originalValue: val, value: val.split('') }})
-    while(oxygenRating.length > 1) {
-        const histo = histogram(oxygenRating.map(v => { const [first, ...rest] = v.value; return first; }));
-        const zeros = histo['0'];
-        const ones = histo['1'];
-        const mostCommon = ones >= zeros ? '1' : '0'; 
 
-        oxygenRating = oxygenRating.filter(v => v.value.shift() === mostCommon).map(v => {return {...v, value: v.value}});
+    let oxygenRating = input.map(val => { return { originalValue: val, value: val.split('') } })
+    while (oxygenRating.length > 1) {
+        const histo = histogram(oxygenRating.map(v => { const [first, ...rest] = v.value; return first; }));
+        const zeros = (histo['0'] ?? 0);
+        const ones = (histo['1'] ?? 0);
+        const mostCommon = ones >= zeros ? 1 : 0;
+
+        oxygenRating = oxygenRating.filter(v => v.value.shift() == mostCommon);
     }
 
-    console.log(oxygenRating);
+    let co2Scrubber = input.map(val => { return { originalValue: val, value: val.split('') } })
+    while (co2Scrubber.length > 1) {
+        const histo = histogram(co2Scrubber.map(v => { const [first, ...rest] = v.value; return first; }));
+        const zeros = (histo['0'] ?? 0);
+        const ones = (histo['1'] ?? 0);
+        const leastCommon = zeros <= ones ? 0 : 1;
 
-    
-    // let co2ScrubberRating = input;
-    // for (let i = 0; i < mostCommon.length; i++) {
-    //     if (oxygenRating.length <= 1) {
-    //         break;
-    //     }
-    //     co2ScrubberRating = co2ScrubberRating.filter(val => val[i] != mostCommon[i]);
-    // }
-    // console.log(parseInt(co2ScrubberRating, 2));
+        co2Scrubber = co2Scrubber.filter(v => v.value.shift() == leastCommon);
 
-    // //console.log(`Solution: ${solution}`);
+    }
+    console.log(`Life support rating: ${parseInt(oxygenRating.shift().originalValue, 2) * parseInt(co2Scrubber.shift().originalValue, 2)}`);
 }
 
 main();
