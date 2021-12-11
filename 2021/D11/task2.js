4e0e8061d93a8b9f42178cd81d92d5072c83afbe
Michel Tosu <michel@waitwhile.com>
2021-12-11 08:39:12 +0100

    solve 2021 D11 T2

---
+++ 2021/D11/task2.js
@@ -0,0 +1,86 @@
+const {getInputForDay} = require('../../util/InputFetcher');
+
+function findOctopusReadyToFlash(grid) {
+    let flashing = []
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[y].length; x++) {
+            if (grid[y][x] > 9) {
+                flashing.push([x,y]);
+            }
+        }
+    }
+
+    return flashing;
+}
+
+async function main() {
+    let input = await (await getInputForDay('2021','11')).split('\n').map(x => x.split('').map(val => Number(val)));
+    let flashCount = 0;
+    let step = 0;
+    while(true) {
+        step++;
+        input = input.map(y => y.map(x => x + 1));
+        let hasFlashed = new Set();
+
+        while (findOctopusReadyToFlash(input).length > 0) {
+            let flashes = findOctopusReadyToFlash(input);
+            flashes = flashes.filter(point => !hasFlashed.has(point.join(',')));
+            if (flashes.length === 0) {
+                if (hasFlashed.size === 100) {
+                    console.log(`Everyone flashed at step: ${step}!`)
+                    return;
+                }
+                break;
+            }
+    
+            while (flashes.length > 0) {
+                let [x,y] = flashes.shift();
+                const point = [x,y].join(',');
+                if(hasFlashed.has(point)) {
+                    continue;
+                }
+    
+                hasFlashed.add(point);
+                flashCount += 1;
+    
+                if (y > 0) {
+                    let yAbove = y - 1;
+                    input[yAbove][x] += 1;
+                    
+                    if (x > 0) {
+                        input[yAbove][x - 1 ] += 1;
+                    }
+                    
+                    if (x < input[y].length - 1) {
+                        input[yAbove][x + 1] += 1;
+                    }
+                }
+                
+                if (x > 0) {
+                    input[y][x - 1] += 1;
+                }
+    
+                if (x < input[y].length - 1) {
+                    input[y][x + 1] += 1;
+    
+                }
+    
+                if (y < input.length - 1) {
+                    input[y + 1][x] += 1;
+    
+                    if (x > 0) {
+                        input[y + 1][x - 1] += 1;
+                    }
+    
+                    if (x < input[y].length - 1) {
+                        input[y + 1][x + 1] += 1;
+                    }
+                } 
+            }
+        };
+        input = input.map(y => y.map(x => x > 9 ? 0 : x));
+    }
+
+}
+
+main();
