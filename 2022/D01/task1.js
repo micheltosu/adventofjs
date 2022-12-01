4f1e3d9c9f564146e41845209f4a05e8409aef95
Michel Tosu <michel@waitwhile.com>
2022-12-01 09:25:28 +0100

    solve 2022-01

---
+++ 2022/D01/task1.js
@@ -0,0 +1,14 @@
+const {getInputForDay} = require('../../util/InputFetcher');
+
+async function main() {
+    const inputBatches = await (await getInputForDay('2022','01'))
+        .split('\n\n')
+        .map(inputBatch => inputBatch.split('\n').map(val => Number(val)));
+
+    const batchSums = inputBatches
+        .map(batch => batch.reduce((acc, x) => acc + x, 0))
+        .sort((a,b) => b - a);
+    console.log(`Calories for elf carrying most calories ${batchSums[0]}`);
+}
+
+main();
