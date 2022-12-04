3e676b41361cb398ae078503fc4855c00beebc1a
Michel Tosu <michel@waitwhile.com>
2022-12-04 09:37:36 +0100

    solve 2022 D04 T2

---
+++ 2022/D04/task2.js
@@ -0,0 +1,24 @@
+const {getInputForDay} = require('../../util/InputFetcher');
+
+async function main() {
+    const input = await (await getInputForDay('2022','04')).split('\n');
+
+    const answer = input.map(line => line.match(/(\d+)-(\d+),(\d+)-(\d+)/))
+        .map(([_, ...matches]) => matches.map(str => Number(str)))
+        .map(([start1, end1, start2, end2]) => {
+            if ((start1 >= start2 && start1 <= end2 ) ||
+            (end1 <= end2 && end1 >= start2) ||
+            (start2 >= start1 && start2 <= end1) ||
+            (end2 <= end1 && end2 >= start1)) {
+                return true
+            }
+
+            return false
+        })
+        .reduce((acc, result) => !!result ? acc+1 : acc);
+
+    console.log(`Number of fully contained assignments: ${answer}`);
+
+}
+
+main();
