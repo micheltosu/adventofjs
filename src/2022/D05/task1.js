77be391a98b29ad801c3a2ea684e2681aa732747
Michel Tosu <michel@tosu.se>
2022-12-10 23:35:09 +0100

    solve 2022 D05 T1

--- src/2022/D05/task1.js
+++
@@ -1,9 +0,0 @@
-const {getInputForDay} = require('../../util/InputFetcher');
-
-async function main() {
-    const input = await (await getInputForDay('2022','05')).split('\n').map(val => Number(val));
-
-}
-
-main();
-    
