cf5652f5b05a3f84e587f58fdb12e158ffa4f908
Michel Tosu <michel@tosu.se>
2022-12-10 23:39:31 +0100

    solve 2022 D05 T2

---
+++ src/2022/D05/task2.ts
@@ -0,0 +1,41 @@
+const {getInputForDay} = require('../../util/InputFetcher');
+
+
+async function main() {
+    const [towers, moves] = (await getInputForDay('2022','05')).split('\n\n').map(s => s.split('\n'));
+
+    const towerDeclaration = towers.pop(); 
+
+    const towerPositions = towerDeclaration
+        .trim()
+        .replace(/ +/g, ',')
+        .split(',')
+        .map(num => towerDeclaration.indexOf(num));
+
+    // console.log(towerPositions);
+
+    const stacks = towerPositions
+        .map(pos => []);
+
+    for (const level of towers.reverse()) {
+        for(const tower in towerPositions) {
+            const towerChar = level.charAt(towerPositions[tower]);
+            if ( towerChar !== ' ') {
+                stacks[tower].push(towerChar);
+            }
+        }
+    }
+    console.log(stacks);
+
+    for (const move of moves) {
+        const [_, num, from, to] = move.match(/move (\d+) from (\d+) to (\d+)/);
+
+        stacks[to - 1].push(...(stacks[from - 1].splice(-num)));
+    }   
+
+    console.log(stacks.flatMap(s => s.slice(-1)).join(''));
+
+}
+
+main();
+    
