76446b60964bc392d87edfb4804b85aa660ea5a3
Michel Tosu <michel@tosu.se>
2023-12-03 23:02:41 +0100

    remove dead fetch method from inputfetcher

--- src/util/InputFetcher.ts
+++ src/util/InputFetcher.ts
@@ -1,4 +1,3 @@
-import superagent = require('superagent');
 import { open } from 'fs/promises';
 
 export const getInputForDay = async (year, day) => {
@@ -6,13 +5,4 @@ export const getInputForDay = async (year, day) => {
     const content = await file.readFile({encoding: "utf-8"});
     file.close();
     return content;
-}
-
-export const fetchInput = function (year, day) {
-    const inputUrl = "https://adventofcode.com/" + year + "/day/" + day + "/input";
-    const authUrl = "https://adventofcode.com/auth/github";
-
-    superagent
-        .get(authUrl)
-        .then(console.log);
 }
