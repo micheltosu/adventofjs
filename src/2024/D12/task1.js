254956bd00eac8a4052b656dddd7fbec020cf404
Michel Tosu <michel@tosu.se>
2024-12-14 14:11:19 +0100

    WIP 2024 D12 T1

--- src/2024/D12/task1.js
+++ src/2024/D12/task1.js
@@ -16,28 +16,32 @@ class RegionNode {
             const node = nodes[coordToKey(this.x, this.y - 1)];
             if (node?.value === this.value) {
                 this.neighbours.UP = node;
-                if (!checked.includes(node)) added.push(...node.addNeighbours(nodes, [...added, ...checked]))
+                if (!(checked.includes(node) || added.includes(node)))
+                    added.push(...node.addNeighbours(nodes, [...added, ...checked]))
             }
         }
         if (this.neighbours.RIGHT === null) {
             const node = nodes[coordToKey(this.x + 1, this.y)];
             if (node?.value === this.value) {
                 this.neighbours.RIGHT = node;
-                if (!checked.includes(node)) added.push(...node.addNeighbours(nodes, [...added, ...checked]))
+                if (!(checked.includes(node) || added.includes(node)))
+                    added.push(...node.addNeighbours(nodes, [...added, ...checked]))
             }
         }
         if (this.neighbours.DOWN === null) {
             const node = nodes[coordToKey(this.x, this.y + 1)];
             if (node?.value === this.value) {
                 this.neighbours.DOWN = node;
-                if (!checked.includes(node)) added.push(...node.addNeighbours(nodes, [...added, ...checked]))
+                if (!(checked.includes(node) || added.includes(node)))
+                    added.push(...node.addNeighbours(nodes, [...added, ...checked]))
             }
         }
         if (this.neighbours.LEFT === null) {
             const node = nodes[coordToKey(this.x - 1, this.y)];
             if (node?.value === this.value) {
                 this.neighbours.LEFT = node;
-                if (!checked.includes(node)) added.push(...node.addNeighbours(nodes, [...added, ...checked]))
+                if (!(checked.includes(node) || added.includes(node)))
+                    added.push(...node.addNeighbours(nodes, [...added, ...checked]))
             }
         }
 
@@ -64,12 +68,16 @@ async function main() {
     while (next) {
         const currNode = nodes[coordToKey(next.x, next.y)];
         const added = currNode.addNeighbours(nodes);
-        for (const { x, y } of added) {
-            leftToScan.splice(leftToScan.findIndex(({ a, b }) => a === x && b === y), 1)
+        for (const { x: xx, y: yy } of added) {
+            const removeNode = leftToScan.findIndex(({ x, y }) => xx === x && yy === y)
+            leftToScan.splice(removeNode, 1)
         }
+        next = leftToScan.shift()
     }
 
-}
+} // Härnäst, lägg till en nod från varje region till en map. 
+// Skriv räkna ihop funktionen.
+// Räkna ihop för varje region.
 
 function coordToKey(x, y) {
     return `${x},${y}`;
