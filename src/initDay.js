bda64a3feb6bbfaf9032603e032ff2087168dd15
Michel Tosu <michel@tosu.se>
2025-12-29 12:49:13 +0100

    make initday produce TS files

--- src/initDay.js
+++ src/initDay.js
@@ -1,5 +1,5 @@
-const fs = require('fs/promises');
-const fetcher = require('../../advent-of-code-fetcher');
+const fs = require("fs/promises");
+const fetcher = require("../../advent-of-code-fetcher");
 
 (async () => {
     const [year, day] = process.argv.slice(2);
@@ -8,12 +8,17 @@ const fetcher = require('../../advent-of-code-fetcher');
         process.exit(1);
     }
 
-    const paddedDay = day.padStart(2, '0');
+    const paddedDay = day.padStart(2, "0");
     const dayPath = `src/${year}/D${paddedDay}`;
     console.log(`Initializing ${year} - Day ${paddedDay}`);
-    await fetcher.saveInputFile(await fetcher.fetchInput(year, paddedDay), dayPath);
+    await fetcher.saveInputFile(
+        await fetcher.fetchInput(year, paddedDay),
+        dayPath
+    );
 
-    await fs.writeFile(`${dayPath}/task1.js`, `const {getInputForDay} = require('../../util/InputFetcher');
+    await fs.writeFile(
+        `${dayPath}/task1.ts`,
+        `import { getInputForDay } from '../../util/InputFetcher';
 
 async function main() {
     const path = process.argv.at(-1) === "test" ? 'test.txt' : "input.txt";
@@ -22,5 +27,7 @@ async function main() {
 }
 
 main();
-    `, { flag: 'wx+' });
-})()
+    `,
+        { flag: "wx+" }
+    );
+})();
