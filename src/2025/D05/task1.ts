7e2ec8bc99f98b1304c9b7dcc546ad153f6160bb
Michel Tosu <michel@tosu.se>
2026-01-03 13:16:17 +0100

    solve 2025 D05 T1

---
+++ src/2025/D05/task1.ts
@@ -0,0 +1,29 @@
+import { getInputForDay } from "../../util/InputFetcher";
+
+async function main() {
+    const path = process.argv.at(-1) === "test" ? "test.txt" : "input.txt";
+    const input = (await getInputForDay("2025", "05", path)).split("\n\n");
+
+    const freshIngredients = input
+        .at(0)
+        .split("\n")
+        .map((line) => line.split("-").map((x) => Number(x)));
+    // .forEach(([start, end]) => {
+    //     for (let i = start; i <= end; i++) {
+    //         freshIngredients.add(i);
+    //     }
+    // });
+
+    console.log("ingredients in fresh list", freshIngredients);
+
+    const freshInventory = input
+        .at(1)
+        .split("\n")
+        .map((x) => Number(x))
+        .filter((x) =>
+            freshIngredients.find(([start, end]) => x >= start && x <= end)
+        );
+    console.log(freshInventory, "length :" + freshInventory.length);
+}
+
+main();
